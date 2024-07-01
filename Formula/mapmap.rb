34ca1fb214fa6b6410bb445d1998b3e0ca7520c3
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2024-07-01 16:22:16 +0200

    Update mapmap.rb

--- Formula/mapmap.rb
+++ Formula/mapmap.rb
@@ -9,7 +9,7 @@ class Mapmap < Formula
       url "https://github.com/ProPro-Productions/MapMap/releases/download/v/mapmap__x64.dmg"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     elsif Hardware::CPU.arm?
-      url "https://github.com/ProPro-Productions/MapMap/releases/download/v/mapmap__aarch64.dmg"
+      url "https://github.com/ProPro-Productions/MapMap/releases/download/1.0.9/mapmap_0.1.0_aarch64.dmg"
       sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     end
   else
