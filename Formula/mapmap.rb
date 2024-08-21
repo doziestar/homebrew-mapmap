23d3631647f61b41639fd5b5db17f907ae377e35
doziestar <chidosiky2015@gmail.com>
2024-08-21 15:46:30 +0000

    Update MapMap to

--- Formula/mapmap.rb
+++ Formula/mapmap.rb
@@ -1,18 +1,16 @@
 class Mapmap < Formula
   desc "MapMap - Write your thoughts, ideas, and plans in a hierarchical tree diagram"
   homepage "https://github.com/ProPro-Productions/MapMap"
-  version "1.0.10"
+  version ""
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/ProPro-Productions/MapMap/releases/download//mapmap_0.1.0_x64.dmg",
-          headers: ["Authorization: token ${HOMEBREW_GITHUB_API_TOKEN}"]
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/mapmap__x64.dmg"
+      sha256 "e3e866883ae7edb8d0f7d5152ec4e974734ae27a1fc73714f014429006dc067e"
     elsif Hardware::CPU.arm?
-      url "https://github.com/ProPro-Productions/MapMap/releases/download//mapmap_0.1.0_aarch64.dmg",
-          headers: ["Authorization: token ${HOMEBREW_GITHUB_API_TOKEN}"]
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/mapmap__aarch64.dmg"
+      sha256 "26234fda624cdcb13f165b8e4f5fbd2cc7a8c595b355563cbcc618823aa8800c"
     end
   else
     odie "Unsupported operating system"
@@ -31,9 +29,6 @@ class Mapmap < Formula
       To start MapMap, you can:
       1. Double click on MapMap.app in your Applications folder
       2. Run `mapmap` in your terminal
-
-      Note: This formula requires a GitHub token with repo access to download the application.
-      Please set the HOMEBREW_GITHUB_API_TOKEN environment variable before installing.
     EOS
   end
 
