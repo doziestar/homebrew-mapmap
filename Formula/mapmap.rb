c98feead393e004f2ec27e1a4081327d691091c9
doziestar <chidosiky2015@gmail.com>
2024-08-22 06:17:16 +0000

    Update MapMap Homebrew formula

--- Formula/mapmap.rb
+++ Formula/mapmap.rb
@@ -1,16 +1,15 @@
 class Mapmap < Formula
   desc "MapMap - Write your thoughts, ideas, and plans in a hierarchical tree diagram"
   homepage "https://github.com/ProPro-Productions/MapMap"
-  version ""
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/mapmap__x64.dmg"
-      sha256 "072728fd43a92dfa97e60ef9a5814d637debf727d30d3fc109ba8758e9a21376"
+      url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/MapMap_x64.dmg"
+      sha256 "a972dd56e66561c9916c0aa1d5b17ffbcf9a0273bc35f4a868ec72fda1d0e21e"
     elsif Hardware::CPU.arm?
-      url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/mapmap__aarch64.dmg"
-      sha256 "9a8c862be6ce3cea34c5edf4487a921bbbd7b2e8606c1aa286e15b07f60ab384"
+      url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/MapMap_aarch64.dmg"
+      sha256 "cdf29c5553c34b579c7fb8ca3ad4122d2b3041083c7b7d813c3bd8084d05e0bc"
     end
   else
     odie "Unsupported operating system"
