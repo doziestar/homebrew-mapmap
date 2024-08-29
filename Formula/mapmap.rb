5a86cd4412a07a5130e567c940a76676a0701e47
doziestar <chidosiky2015@gmail.com>
2024-08-29 13:59:36 +0000

    Update MapMap Homebrew formula

--- Formula/mapmap.rb
+++ Formula/mapmap.rb
@@ -6,10 +6,10 @@ class Mapmap < Formula
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/MapMap_x64.dmg"
-      sha256 "aae1a0538f6f3e09eb84eaf4f920407019c82c4076e67bbd2717d64220614adb"
+      sha256 "6da33a03be79252e31ecf9815b938af55315c4f1ff031a3b6e2aee560d8a1048"
     elsif Hardware::CPU.arm?
       url "https://github.com/doziestar/homebrew-mapmap/raw/main/download/MapMap_aarch64.dmg"
-      sha256 "af0f0a9bb1c175bb4f228691e2b030d1886b9a6a208831d1c130fc1e6e350952"
+      sha256 "2d289da1e4a30bc182ecee28dfa8ff2602629b0155d3f7f71f31c6337919a371"
     end
   else
     odie "Unsupported operating system"
